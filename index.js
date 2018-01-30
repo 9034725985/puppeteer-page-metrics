10b67603c6bee6f3f484295b282a5063e3ba086a
twclark0 <tylerc644@gmail.com>
2018-01-29 22:35:18 -0700

    Get all response metrics and log them out

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const calcCss = require('./calcCss.js')
 const getPageMetrics = async () => {
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
-  await page._client.send('Page.enable')
   await page._client.send('DOM.enable')
   await page._client.send('CSS.enable')
   await page._client.send('CSS.startRuleUsageTracking')
-  await page._client.send('Performance.enable');
+  await page._client.send('Performance.enable')
 
   const stylesheets = []
   page._client.on('CSS.styleSheetAdded', s => stylesheets.push(s.header))
 
   await page.goto('https://www.google.com/')
-  const { metrics } = await page._client.send('Performance.getMetrics')
-  const JSUsedSize = metrics.find(x => x.name === 'JSHeapUsedSize').value
-  const JSTotalSize = metrics.find(x => x.name === 'JSHeapTotalSize').value
+  const response = await page._client.send('Performance.getMetrics')
+  const JSUsedSize = response.metrics.find(x => x.name === 'JSHeapUsedSize').value
+  const JSTotalSize = response.metrics.find(x => x.name === 'JSHeapTotalSize').value
   const unusedJS = Math.round((JSUsedSize / JSTotalSize) * 100)
   const perf = await page.evaluate(_ => ({
     firstPaint:
@@ -27,7 +26,7 @@ const getPageMetrics = async () => {
   const { ruleUsage } = await page._client.send('CSS.stopRuleUsageTracking')
 
   const unusedCSS = calcCss.unusedCss(stylesheets, ruleUsage)
-
+  console.log(response)
   console.log(`The page's first paint time is ${perf.firstPaint}ms`)
   console.log(
     `${unusedCSS}% of CSS is unused, ${

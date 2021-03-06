70aba7757c224e0f659488fa6aec5bcca3899bac
Kushal Hada <kushaldeveloper@gmail.com>
2021-03-06 18:00:24 -0500

    remove perf
    I don't even know what the point of this is anymore
    but at least it will run

--- index.js
+++ index.js
@@ -36,41 +36,71 @@ const addresses = [
 ]
 
 async function getPageMetrics(urlstring) {
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
-  await page._client.send('DOM.enable')
-  await page._client.send('CSS.enable')
-  await page._client.send('CSS.startRuleUsageTracking')
-  await page._client.send('Performance.enable')
+  try {
+    const args = [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--blink-settings=imagesEnabled=true",
+    ]
 
-  const stylesheets = []
-  page._client.on('CSS.styleSheetAdded', s => stylesheets.push(s.header))
+    const options = {
+      args,
+      headless: true,
+      ignoreHTTPSErrors: true
+    }
 
-  await page.goto(urlstring)
-  const response = await page._client.send('Performance.getMetrics')
-  const JSUsedSize = response.metrics.find(x => x.name === 'JSHeapUsedSize').value
-  const JSTotalSize = response.metrics.find(x => x.name === 'JSHeapTotalSize').value
-  const unusedJS = Math.round((JSUsedSize / JSTotalSize) * 100)
-  const perf = await page.evaluate(_ => ({
-    firstPaint:
-      chrome.loadTimes().firstPaintTime * 1000 -
-      performance.timing.navigationStart
-  }))
+    const browser = await puppeteer.launch(options)
+    const page = await browser.newPage()
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    })
+    await page._client.send('DOM.enable')
+    await page._client.send('CSS.enable')
+    await page._client.send('CSS.startRuleUsageTracking')
+    await page._client.send('Performance.enable')
 
-  const { ruleUsage } = await page._client.send('CSS.stopRuleUsageTracking')
+    const stylesheets = []
+    page._client.on('CSS.styleSheetAdded', s => stylesheets.push(s.header))
 
-  const unusedCSS = calcCss.unusedCss(stylesheets, ruleUsage)
-  console.log(`For ${urlstring}: `)
-  // console.log(response)
-  console.log(`The page's first paint time is ${perf.firstPaint}ms`)
-  console.log(
-    `${unusedCSS}% of CSS is unused, ${stylesheets.length
-    } total stylesheets`
-  )
-  console.log(`${unusedJS}% of JS is unused`)
-  console.log(`End of report for ${urlstring}`)
-  await browser.close()
+    const start_time = new Date()
+    await page.goto(urlstring)
+    const end_time = new Date()
+
+    console.log({ start_time })
+    console.log({ end_time })
+    console.log({ total_time: end_time - start_time })
+
+    const response = await page._client.send('Performance.getMetrics')
+    const JSUsedSize = response.metrics.find(x => x.name === 'JSHeapUsedSize').value
+    const JSTotalSize = response.metrics.find(x => x.name === 'JSHeapTotalSize').value
+    const unusedJS = Math.round((JSUsedSize / JSTotalSize) * 100)
+    // const perf = await page.evaluate(_ => ({
+    //   firstPaint:
+    //     chrome.loadTimes().firstPaintTime * 1000 -
+    //     performance.timing.navigationStart
+    // }))
+
+    const { ruleUsage } = await page._client.send('CSS.stopRuleUsageTracking')
+
+    const unusedCSS = calcCss.unusedCss(stylesheets, ruleUsage)
+    console.log(`For ${urlstring}: `)
+    // console.log(response)
+    // console.log(`The page's first paint time is ${perf.firstPaint}ms`)
+    console.log(
+      `${unusedCSS}% of CSS is unused, ${stylesheets.length
+      } total stylesheets`
+    )
+    console.log(`${unusedJS}% of JS is unused`)
+    console.log(`End of report for ${urlstring}`)
+    await browser.close()
+  } catch (error) {
+    console.log({ error })
+  }
 }
+
+console.log(`${new Date()}`)
+console.log('\r\n')
 
 addresses.forEach(address => {
   getPageMetrics(address)
